26c59cd038be26c1744f6a840cbfb2e4befea70d
Logan Coates <coates.logan1125@gmail.com>
2021-08-20 14:22:12 -0600

    added a couple guns

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Footer.css';
 
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
+      <footer class="py-5 bg-dark">
         <div class="container">
           <p class="m-0 text-center text-white">
             Copyright &copy; Your Website 2020
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Krig from "../components/images/krig-6.jpg";
+import Bullfrog from "../components/images/bullfrog.jpg";
+import Swiss from "../components/images/Swiss.png";
 
 function Home() {
   return (
@@ -6,19 +9,67 @@ function Home() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            <img class="img-fluid rounded mb-4 mb-lg-0" src={Krig} alt="" />
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
+            <h1 class="font-weight-light">Krig-6 </h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              The Krig-6 is the most used assault rifle in the game right now.
+              With low recoil and a fast time to kill it is the meta AR right
+              now. The best build for it comes from pro warzone player Newbz.
+              <br />
+              <br />
+              <li> Agency suppressor</li>
+              <li>Mil Spec Barrel</li>
+              <li>3X Optic</li>
+              <li>Feild Agent Grip</li>
+              <li>60 Rnd Mag</li>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img class="img-fluid rounded mb-4 mb-lg-0" src={Swiss} alt="" />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Swiss </h1>
+            <p>
+              The Swiss is the most used Sniper rifle in the game right now. The
+              low flinch with fast rate of fire and aim down sight speed makes
+              this the best sniper to use. The best build for it comes from pro
+              warzone player Newbz.
+              <br />
+              <br />
+              <li> Agency suppressor</li>
+              <li>Mil Spec Barrel</li>
+              <li>3X Optic</li>
+              <li>Feild Agent Grip</li>
+              <li>60 Rnd Mag</li>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img class="img-fluid rounded mb-4 mb-lg-0" src={Bullfrog} alt="" />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Krig-6 </h1>
+            <p>
+              The Bullfrog is one of many viable Subs you can use as a
+              secondary. is the most used assault rifle in the game right now.
+              With low recoil and a fast time to kill it is the meta AR right
+              now. The best build for it comes from pro warzone player Newbz.
+              <br />
+              <br />
+              <li> Agency suppressor</li>
+              <li>Mil Spec Barrel</li>
+              <li>3X Optic</li>
+              <li>Feild Agent Grip</li>
+              <li>60 Rnd Mag</li>
             </p>
           </div>
         </div>
